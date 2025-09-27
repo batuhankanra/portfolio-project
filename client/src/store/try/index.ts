a204eebc33a66260134eab362b054d3b8541de8a
batuhan <batuhan_kanra_60@hotmail.com>
2025-09-27 12:41:05 +0300

    client start

---
+++ client/src/store/try/index.ts
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const initialState={
+    data:"sa"
+}
+const trySlice=createSlice({
+    name:"selam",
+    initialState,
+    reducers:{}
+})
+export default trySlice.reducer
